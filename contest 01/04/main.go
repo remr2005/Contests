10a42cb09e73ceaf38831a0274f8e31e1bc30d30
Kemran <109353270+remr2005@users.noreply.github.com>
2023-10-10 00:07:04 +0300

    Delete contest 01 directory

--- contest 01/04/main.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-
-func main() {
-    var a int
-    var b int
-    fmt.Scan(&a,&b)
-    fmt.Println(a*b)
-}
--- contest 01/05/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{	
-	float a, b;
-	std::cin >> a;
-	std::cin >> b;
-	std::cout << a/(b*b);
-}
