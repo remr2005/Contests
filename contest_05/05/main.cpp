20b52d7a10c973d5b3a0eefe3c2692eea46a9ea3
Kemran <109353270+remr2005@users.noreply.github.com>
2023-11-15 15:17:49 +0300

    Update main.cpp

--- contest_05/05/main.cpp
+++ contest_05/05/main.cpp
@@ -12,7 +12,7 @@ public:
     std::string stringfi() {
         std::string res;
         for (auto i : this->arr) res = std::to_string(i) + res;
-        if (this->znak and res != "0") { res = "-" + res; /*std::cout << 1 << '\n'; */ }
+        if (this->znak and res != "0") { res = "-" + res;  }
         return res;
     }
 
@@ -40,7 +40,7 @@ public:
     {
         if (this->arr.size() == b.arr.size()) {
             for (int i = this->arr.size() - 1; i >= 0;i--) {
-                if (this->arr[i] > b.arr[i]) return true;
+                if (this->arr[i] >= b.arr[i]) return true;
                 else return false;
             }
         }
@@ -79,104 +79,223 @@ std::ostream& operator<<(std::ostream& out, const Bigint& num) {
     return out;
 }
 
-Bigint operator + ( Bigint& a,  Bigint& b){   
+Bigint operator +(Bigint a, Bigint b){
     Bigint res;
-    if ((a.znak or b.znak) and !(a.znak and b.znak)) {
-        /*Серьезность	Код	Описание	Проект	Файл	Строка	Состояние подавления
-Ошибка	C2676	бинарный "-": "Bigint" не определяет этот оператор или преобразование к типу приемлемо к встроенному оператору	contest_5	C:\Users\USER\source\repos\contest_5\contest_5\contest_5.cpp	408	
-*/
-    }
-    int len;
-    int buf = 0;
-    if (a.arr.size() > b.arr.size()) len = a.arr.size();
-    else len = b.arr.size();
-    for (int i = 0; i < len; i++) {
-        if (i >= a.arr.size()) {
-            a.arr.push_back(0);
+    if (a.znak != 1 and b.znak != 1) {
+        int carry = 0;
+        for (size_t i = 0; i < std::max(a.arr.size(), b.arr.size()) || carry; ++i) {
+            if (i == a.arr.size())
+                a.arr.push_back(0);
+            a.arr[i] += carry + (i < b.arr.size() ? b.arr[i] : 0);
+            carry = a.arr[i] >= 10;
+            if (carry)  a.arr[i] -= 10;
         }
-        if (i >= b.arr.size()) {
-            b.arr.push_back(0);
-        }
-        res.arr.push_back((buf + a.arr[i] + b.arr[i])%10);
-        buf = (buf + a.arr[i] + b.arr[i]) / 10;
+        res = a;
     }
-    if (buf != 0) res.arr.push_back(buf);
-    if (a.znak and b.znak) res.znak = true;
+    else if (a.znak and b.znak) {
+        a.znak = false;
+        b.znak = false;
+        res = a + b;
+        res.znak = true;
+    }
+    else if (a.znak != 1 and b.znak) {
+        b.znak = false;
+        if (a > b) {
+            b.znak = true;
+            int len;
+            int buf = 0;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf + a.arr[i] - b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf + a.arr[i] - b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf + a.arr[i] - b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.str = res.stringfi();
+            return res;
+        }
+        else {
+            int len;
+            int buf = 0;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf - a.arr[i] + b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf - a.arr[i] + b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf - a.arr[i] + b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.znak = true;
+            res.str = res.stringfi();
+            return res;
+        }
+    }
+    else if (a.znak and b.znak != 1) {
+        a.znak = false;
+        if (a > b) {
+            a.znak = true;
+            int len;
+            int buf = 0;
+            res.znak = true;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf + a.arr[i] - b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf + a.arr[i] - b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf + a.arr[i] - b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.str = res.stringfi();
+            return res;
+        }
+        else {
+            int len;
+            int buf = 0;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf - a.arr[i] + b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf - a.arr[i] + b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf - a.arr[i] + b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.znak = true;
+            res.str = res.stringfi();
+            return res;
+        }
+    }
     res.str = res.stringfi();
     return res;
 }
 
-Bigint operator - (Bigint& a, Bigint& b)
-{   
+Bigint operator -(Bigint a, Bigint b) {
     Bigint res;
-    if (a.znak and b.znak == false) {
-        a.znak = false;
-        res = (a + b);
-        res.znak = true;
-        res.str = res.stringfi();
-        return res;
+    if (a.znak != 1 and b.znak != 1) {
+        if (a > b) {
+            int len;
+            int buf = 0;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf + a.arr[i] - b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf + a.arr[i] - b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf + a.arr[i] - b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.str = res.stringfi();
+            return res;
+        }
+        else {
+            int len;
+            int buf = 0;
+            if (a.arr.size() > b.arr.size()) len = a.arr.size();
+            else len = b.arr.size();
+            for (int i = 0; i < len; i++) {
+                if (i >= a.arr.size()) {
+                    a.arr.push_back(0);
+                }
+                if (i >= b.arr.size()) {
+                    b.arr.push_back(0);
+                }
+
+                if ((-buf - a.arr[i] + b.arr[i]) < 0) {
+                    res.arr.push_back(10 - buf - a.arr[i] + b.arr[i]);
+                    buf = 1;
+                }
+                else {
+                    res.arr.push_back(-buf - a.arr[i] + b.arr[i]);
+                    buf = 0;
+                }
+            }
+
+            while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
+            res.znak = true;
+            res.str = res.stringfi();
+            return res;
+        }
     }
-    else if (a.znak and b.znak) {
+    if (a.znak and b.znak != 1) {
+        a.znak = false;
+        res = a + b;
+        res.znak = true;
+    }
+    if (a.znak and b.znak) {
         b.znak = false;
         a.znak = false;
         res = b - a;
-        return res;
     }
-    else if (a > b) {
-        int len;
-        int buf = 0;
-        if (a.arr.size() > b.arr.size()) len = a.arr.size();
-        else len = b.arr.size();
-        for (int i = 0; i < len; i++) {
-            if (i >= a.arr.size()) {
-                a.arr.push_back(0);
-            }
-            if (i >= b.arr.size()) {
-                b.arr.push_back(0);
-            }
-
-            if ((-buf + a.arr[i] - b.arr[i]) < 0) {
-                res.arr.push_back(10 - buf + a.arr[i] - b.arr[i]);
-                buf = 1;
-            }
-            else {
-                res.arr.push_back(-buf + a.arr[i] - b.arr[i]);
-                buf = 0;
-            }
-        }
-
-        while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
-        res.str = res.stringfi();
-        return res;
+    if (a.znak != 1 and b.znak) {
+        b.znak = false;
+        res = a + b;
     }
-    else {
-        int len;
-        int buf = 0;
-        if (a.arr.size() > b.arr.size()) len = a.arr.size();
-        else len = b.arr.size();
-        for (int i = 0; i < len; i++) {
-            if (i >= a.arr.size()) {
-                a.arr.push_back(0);
-            }
-            if (i >= b.arr.size()) {
-                b.arr.push_back(0);
-            }
-
-            if ((-buf - a.arr[i] + b.arr[i]) < 0) {
-                res.arr.push_back(10 - buf - a.arr[i] + b.arr[i]);
-                buf = 1;
-            }
-            else {
-                res.arr.push_back(-buf - a.arr[i] + b.arr[i]);
-                buf = 0;
-            }
-        }
-
-        while (res.arr.size() > 1 && res.arr.back() == 0) res.arr.pop_back();
-        res.znak = true;
-        res.str = res.stringfi();
-        return res;
-    }
+    res.str = res.stringfi();
+    return res;
 }
 
 int main() {
@@ -185,9 +304,6 @@ int main() {
 
     Bigint first(a);
     Bigint second(b);
-    Bigint c;
-    c = (first - second);
-   // std::cout << first << std::endl;
     std::cout << "a + b = " << (first + second) << std::endl;
     std::cout << "a - b = " << (first - second) << std::endl;
     /*
