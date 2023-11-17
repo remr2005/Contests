e83b96aab5117570354c5b5789ed8ead313f6d4e
Kemran <109353270+remr2005@users.noreply.github.com>
2023-11-17 18:26:49 +0300

    Update main.cpp

--- contest_06/02/main.cpp
+++ contest_06/02/main.cpp
@@ -1,1 +1,39 @@
-// Здесь обязательно что-то появиться, но потом...
+#include<cmath>
+
+class Employee {
+public:
+    std::string name;
+    std::string role;
+    int money;
+    Employee(std::string name, std::string role, int money) {
+        this->name = name;
+        this->role = role;
+        this->money = money;
+    }
+    int bonus(double proc = 0) {
+        double buf = this->money;
+        if (this->role == "manager" and proc < 0.10) {
+            buf = round(buf * 0.10);
+        }
+        else{
+            buf = round(buf* proc);
+        }
+        return buf;
+    }
+    int salary() {
+        return this->money + this->bonus(bonuses[this->role]);
+    }
+
+};
+
+std::ostream& operator<<(std::ostream& out, Employee& per) {
+    out << per.name <<" ("<< per.role <<"): " << per.salary();
+    return out;
+}
+
+class Manager : public Employee {
+public:
+    Manager(std::string name, int money = 16242)
+        :Employee(name, "manager", money) {
+    }
+};
